aed9f00f1ea1e5cdde9299a2eb3bdfa81f06f971
Hirok Sengupta <senguptahirok@gmail.com>
2024-08-11 22:20:38 +0530

    debugging

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.get('/api/whoami',function(req,res){
-  console.log('language' + Accept-Language);
-  res.json({"ipaddress": req.ip, "language": Accept-Language, "software": software});
+  console.log('language = ' + Accept-Language);
+//  res.json({"ipaddress": req.ip, "language": Accept-Language, "software": software});
+   res.json({"ipaddress": req.ip});
 });
 
 // listen for requests :)
